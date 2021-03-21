84e3c8b4cb1f651381e8bb0c3220314368fd02ce
Matthew Lowe <matthewjlowe11@gmail.com>
2021-03-21 11:38:32 -0400

    Create array for each team containing all franchise info

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,12 +15,11 @@ fetch('https://statsapi.web.nhl.com/api/v1/teams')
       let team = data.teams;
       
       team.map((i) => {
-        div = i.abbreviation;
-        createDivElement(div, i.name);
-        h2tag = 'h2' + i.abbreviation;
-        createH2Element(h2tag, i.name, i.abbreviation);
-        
         createTeamArray(i.abbreviation, i.id);
+        let div = i.abbreviation;
+        createDivElement(div, i.name);
+        let h2tag = 'h2' + i.abbreviation;
+        createH2Element(h2tag, i.name, i.abbreviation);
       })
   }).then(() => {
     console.log("Do some stuff...")
@@ -50,19 +49,37 @@ function createH2Element(id, value, div) {
   divWrap.append(h2);
 }
 
+// id is id value assigned to h2
+// value is value of h2
+function createPElement(title,id, value, div) {
+  let p = document.createElement('p');
+  p.id = id;
+  p.append(`${title} : ${value}`);
+  
+  //Append to div
+  let divWrap = document.getElementById(div);
+  divWrap.append(p);
+}
+
 function createTeamArray(team, id) {
   console.log(team, id);
   
-  teamArray = [team, {}];
+  let teamArray = [team];
   
   fetch('https://statsapi.web.nhl.com/api/v1/teams/' + id)
     .then(response => response.json())
     .then((data) => {
       let teamData = data.teams;
-      console.log(teamData);
+      let teamDiv = document.getElementById(team);
       
       teamData.map((i) => {
+        teamArray.push(i);
+        console.log(i);
         
+        createPElement("Venue", id, i.venue.name, team);
+        createPElement("Conference", id, i.conference.name, team);
+        createPElement("Division", id, i.division.name, team);
+        createPElement("Website", id, i.officialSiteUrl, team);
       })
     })
 }
